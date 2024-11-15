876b778bb7848fb5e92cc0d7d3248b09293a7dc3
TheQiBandit <164599241+TheQiBandit@users.noreply.github.com>
2024-11-15 15:17:01 +0200

    Change logic into two classes
    Discarded most of the previous logic written, and
    wrote two new classes one for board and one for
    players.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,70 +1,105 @@
-# Game back-end logic
-
 require 'io/console'
 
-class TicLogic
+class Board
   GRID = [
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9]
-      ]
+  ]
       
-      attr_accessor :mutable_grid
+  attr_accessor :mutable_grid
 
-     def initialize
-      @mutable_grid = GRID             
-     end 
+  def initialize
+    @mutable_grid = GRID.map do |inner_array|
+            inner_array.map do |position|
+              position = " "
+            end
+    end             
+  end 
 
-     def player_turn
+  def help_board
+    display_options = GRID.map do |arr|
+      arr.map { |element| element }.join(" ")
+    end
 
-       puts "Choose a NUMBER to play as X!!"
+    puts "Choose any empty spot/number to play your turn at."
+    puts "-----"
+    puts display_options
+    puts "-----"
 
-       get_number = STDIN.noecho { |i| i.gets}.chomp.to_i
-       play_turn = GRID.flatten.index(get_number)
+  end
 
-       play_turn
+  def display_board
+    puts "-------------"
+    puts "| #{mutable_grid[0][0]} : #{mutable_grid[0][1]} : #{mutable_grid[0][2]} |"
+    puts "............."
+    puts "| #{mutable_grid[1][0]} : #{mutable_grid[1][1]} : #{mutable_grid[1][2]} |"
+    puts "............."
+    puts "| #{mutable_grid[2][0]} : #{mutable_grid[2][1]} : #{mutable_grid[2][2]} |"
+    puts "-------------"
 
-     end
+  end
 
-     def grid_change(grid_position)
+  def update_board
+    
+  end
 
-      if mutable_grid.flatten.at(grid_position) == "X" || mutable_grid.flatten.at(grid_position) == "O"
-        player_turn
-
-      else
-
-        case grid_position
-        when 0..2
-          mutable_grid[0][grid_position] = "X"
-        when 3..5
-          mutable_grid[1][grid_position - 3] = "X"
-        when 6..8
-          mutable_grid[2][grid_position - 6] = "X"
-        else
-          puts "Please enter a valid choice!"
-        end
-        
-      end
-
-      display_board(mutable_grid)
-
-     end
-
-     def display_board(mutable_grid = GRID)
-       
-      grid_string = mutable_grid.map do |array|
-        array.map { |elem| elem }.join(" ")
-      end
-      
-      puts grid_string
-
-     end
-
-     def modified_grid
-       display_board(mutable_grid)
-     end
-          
 end
 
-first_game = TicLogic.new
-first_game.grid_change(first_game.player_turn)
+# first_game = Board.new
+# first_game.display_board
+
+
+class Player
+  attr_accessor :players, :turns_played
+  attr_reader :board
+  
+  def initialize
+    @players = []
+    @turns_played = 0
+  end
+
+  def choose_weapon
+    puts "Choose X or O!!"
+
+    choice = STDIN.noecho { |i| i.gets }.chomp.upcase
+
+    if choice != "X" && choice != "O"
+      puts "Unacceptable, only X's or O's please!"
+    else
+      players.push(choice)
+    end
+
+  end
+
+  def distribute_players
+     
+    if players[0] == "X"
+      players.push("O")
+    else
+      players.push("X") unless players[0] == nil
+    end
+    
+    players
+  end
+
+  def choose_position
+    
+    @board.help_board
+    chosen_position = STDIN.noecho { |i| i.gets }.chomp.to_i
+
+    if chosen_position.between?(1, 9)
+      chosen_position
+    else
+      puts "Please only choose a number from 1 - 9!"
+      choose_position
+    end
+
+  end
+
+end
+
+test_play = Player.new
+test_play.choose_weapon
+test_play.distribute_players
+puts test_play.choose_position
