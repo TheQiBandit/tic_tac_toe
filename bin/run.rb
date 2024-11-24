4ce636c80c88fd1d7bc6a0790c9130c06bcce3b9
TheQiBandit <164599241+TheQiBandit@users.noreply.github.com>
2024-11-24 09:36:22 +0200

    Fix require_relative command in bin/run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,7 @@
-require_all "./lib"
+require_relative "../lib/cli"
+require_relative "../lib/players"
+require_relative "../lib/board"
+require_relative "../lib/win_tie"
 
 # create CLI
 cli = CLI.new
